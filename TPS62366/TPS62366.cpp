c403ad00ecf4118da5a5e35b164460587f444dea
Mark Jessop <darkside@Marks-MacBook-Pro-4.local>
2013-08-03 19:09:43 +0930

    Initial commit.

---
+++ TPS62366/TPS62366.cpp
@@ -0,0 +1,21 @@
+/*
+  TPS62366.cpp - Texas Instruments TPS62366x Control Library
+  Copyright (c) 2013 Mark Jessop.  All right reserved.
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  For a full copy of the GNU General Public License, 
+  see <http://www.gnu.org/licenses/>.
+*/
+
+#include <Arduino.h> 
+#include "TPS62366.h"
+
